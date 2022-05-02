8310cc0f81cdadba174b6637c3b3029877bce8c1
giunashvili <giorgi.official@yahoo.com>
2022-05-02 11:37:24 +0400

    feat: add Country model

---
+++ src/models/Country.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const {Schema} = mongoose;
+
+const countrySchema = new Schema({
+    code: String,
+    name: {
+        en: String,
+        ka: String,
+    },
+});
+
+const Country = mongoose.model('Country', countrySchema);
+
+export default Country;
